65f8c24bdcec89a4c38fcab0ff6131e3ba6d6771
jdevine82 <jdevine82@gmail.com>
2017-11-07 04:28:10 +0000

    changed number types

--- db/migrate/20171018014753_modifyanimals.rb
+++ db/migrate/20171018014753_modifyanimals.rb
@@ -3,9 +3,9 @@ class Modifyanimals < ActiveRecord::Migration
     remove_column :animals, :name
     remove_column :animals, :npws
     remove_column :animals, :family
-    add_column :animals, :nwpsid, :number
+    add_column :animals, :nwpsid, :integer
     add_column :animals, :animals, :text
     add_column :animals, :family, :text
-    add_column :animals, :code, :number
+    add_column :animals, :code, :integer
   end
 end
